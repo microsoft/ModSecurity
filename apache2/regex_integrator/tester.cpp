d604ed2d798d241ac6e9b0d30d9af6b842c5a071
Wei Bai <baiwei0427@users.noreply.github.com>
2018-09-04 14:39:25 +0800

    Add execution options

--- apache2/regex_integrator/tester.cpp
+++ apache2/regex_integrator/tester.cpp
@@ -8,11 +8,11 @@
 
 #define OVECCOUNT 30
 
-
 int test(
-    const char * pattern_str, 
-    const char * subject_str, 
+    const char *pattern_str, 
+    const char *subject_str, 
     int comp_opt,
+    int exec_opt,
     int expect) {
     
     // pattern_str = "a$";
@@ -23,20 +23,21 @@ int test(
     int ret = 0;
 
     // comp_opt = RI_COMP_MULTILINE | RI_COMP_CASELESS;
-    fprintf(stderr, "TEST START \n\tpattern(%s)\n\tsubject(%s)\n\tcompile option(%d)\n\texpect(%d)\n", 
+    fprintf(stderr, "TEST START \n\tpattern(%s)\n\tsubject(%s)\n\tcompile option(%d)\n\texection option(%d)\n\texpect(%d)\n", 
         pattern_str, 
         subject_str, 
         comp_opt, 
+        exec_opt,
         expect);
     ri_set_log_level(RI_LOG_DEBUG);
 
     ret = ri_create(&regex, pattern_str, comp_opt, NULL,  NULL, &log);
     if (ret != RI_SUCCESS) {
-        fprintf(stderr,"%s\n", log);
+        fprintf(stderr, "%s\n", log);
         goto test_error;
     }
 
-    ret = ri_exec(regex, subject_str, strlen(subject_str), 0, 0, NULL, ovector, OVECCOUNT, NULL);
+    ret = ri_exec(regex, subject_str, strlen(subject_str), 0, exec_opt, NULL, ovector, OVECCOUNT, NULL);
     if (ret < 0) {
         fprintf(stderr, "%s\n", ri_get_error_msg(ret));
     }
@@ -50,6 +51,7 @@ int test(
     }
     fprintf(stderr, "TEST FINISH\n");
     return 0;
+
 test_error:
     if (regex) {
         ri_free(regex);
@@ -62,33 +64,37 @@ int main() {
     ri_init();
 
     // Normal match
-    if (test("abc", "abc", 0, 1) != 0) {
+    if (test("abc", "abc", RI_COMP_DEFAULT, RI_EXEC_DEFAULT, 1) != 0) {
         goto exit_error;
     }
     
     // Normal un-match
-    if (test("def", "abc", 0, -1) != 0) {
+    if (test("def", "abc", RI_COMP_DEFAULT, RI_EXEC_DEFAULT, -1) != 0) {
         goto exit_error;
     }
 
     // Compile error
-    if (test("{abc(:", "abc", 0, -1) != -1) {
+    if (test("{abc(:", "abc", RI_COMP_DEFAULT, RI_EXEC_DEFAULT, -1) != -1) {
         goto exit_error;
     }
 
     // Large match
-    if (test("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 0, 1) != 0) {
+    if (test("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 
+        RI_COMP_DEFAULT, 
+        RI_EXEC_DEFAULT,
+        1) != 0) {
         goto exit_error;
     }
 
     // Newline support
-    if (test("b$", "b\nb", RI_COMP_MULTILINE | RI_COMP_CASELESS, 1) != 0) {
+    if (test("b$", "b\nb", RI_COMP_MULTILINE | RI_COMP_CASELESS, RI_EXEC_DEFAULT, 1) != 0) {
         goto exit_error;
     }
 
-
     ri_release();
     return 0;
+
 exit_error:
     ri_release();
     return -1;
